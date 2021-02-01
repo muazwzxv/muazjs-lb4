1ad37c24cc9d0da623082faf55c9f88da9eb7d68
Muaz Wazir <muazwazir@gmail.com>
2021-02-02 00:14:26 +0800

    authorization provider

---
+++ src/providers/user-permissions.provider.ts
@@ -0,0 +1,26 @@
+import {Provider} from '@loopback/context';
+import {intersection} from 'lodash';
+import {
+  PermissionKey,
+  RequiredPermisisons,
+  UserPermissionfn,
+} from '../authorization';
+
+export class UserPermissionsProvider implements Provider<UserPermissionfn> {
+  constructor() {}
+
+  value(): UserPermissionfn {
+    return (userPermisisons, requiredPermissions) =>
+      this.action(userPermisisons, requiredPermissions);
+  }
+
+  action(
+    userPermission: PermissionKey[],
+    requiredPermissions: RequiredPermisisons,
+  ): boolean {
+    return (
+      intersection(userPermission, requiredPermissions.required).length ===
+      requiredPermissions.required.length
+    );
+  }
+}
