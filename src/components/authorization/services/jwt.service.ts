acc9a3bad89da2fb7144566cb395f34da5637807
Muaz Wazir <muazwazir@gmail.com>
2021-02-04 15:57:56 +0800

    getToken function

--- src/components/authorization/services/jwt.service.ts
+++ src/components/authorization/services/jwt.service.ts
@@ -3,14 +3,16 @@ import {inject} from '@loopback/core';
 import {repository} from '@loopback/repository';
 import {HttpErrors} from '@loopback/rest';
 import {UserProfile} from '@loopback/security';
+import {toJSON} from '@loopback/testlab';
 import * as jwt from 'jsonwebtoken';
 import * as _ from 'lodash';
 import {promisify} from 'util';
 import {TokenServiceConstant} from '../../../keys';
 import {UserRepository} from '../../../repositories';
+import {Credentials} from '../types';
 
 // Convert callbacks to Promise1
-// const signAsync = promisify(jwt.sign);
+const signAsync = promisify(jwt.sign);
 const verifyAsync = promisify(jwt.verify);
 
 export class JWTService implements TokenService {
@@ -44,11 +46,32 @@ export class JWTService implements TokenService {
     return user;
   }
 
-  test() {
-    console.log('Hello world');
+  async generateToken(user: UserProfile): Promise<string> {
+    const token = await signAsync(user, this.jwtSecret);
+
+    return token as string;
   }
 
-  async generateToken(user: UserProfile): Promise<string> {
-    return '';
+  async getToken(credential: Credentials): Promise<string> {
+    const found = await this.userRepository.findOne({
+      where: {email: credential.email},
+    });
+
+    if (!found)
+      throw new HttpErrors['Not Found'](
+        `user with email ${credential.email} not found`,
+      );
+
+    if (credential.password !== found.password)
+      throw new HttpErrors.Unauthorized('The credentials are not correct');
+
+    const user = _.pick(toJSON(found), [
+      'email',
+      'name',
+      'permissions',
+    ]) as UserProfile;
+
+    const token = await this.generateToken(user);
+    return token;
   }
 }
